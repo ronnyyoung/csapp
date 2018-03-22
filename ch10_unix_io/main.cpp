1a4f11d6dc9cb68e22c87e38a13f2c2fc7660c6a
Yang Yansheng <yangyansheng@sensenets.com>
2018-03-22 16:56:51 +0800

    初步完成了第10章的笔记

--- ch10_unix_io/main.cpp
+++ ch10_unix_io/main.cpp
@@ -11,8 +11,23 @@
 
 
 int main() {
-    char buf[1024];
-    ssize_t n_read = read(STDIN_FILENO, buf, 1024);
-    std::cout << n_read << std::endl;
+    int fd = open("/home/yansheng/Pictures/yys.jpg", O_RDONLY, 0);
+    off_t file_end = lseek(fd, 0, SEEK_END);
+    off_t file_start = lseek(fd, 0, SEEK_SET);
+    size_t file_size = file_end - file_start;
+
+    char *buffer = new char[file_size];
+    read(fd, buffer, file_size);
+
+    mode_t file_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+    mode_t mask = S_IWGRP | S_IWOTH;
+    umask(mask);
+    int pic_fd = open("tmp.jpg", O_CREAT|O_RDWR, file_mode);
+    write(pic_fd, buffer, file_size);
+    close(fd);
+    close(pic_fd);
+
+    delete[] buffer;
+
     return 0;
 }
